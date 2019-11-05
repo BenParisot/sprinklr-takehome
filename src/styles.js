1db1e1826811e89a52f658d683601b42925a516e
Ben Parisot <me@benparisot.com>
2019-11-04 19:29:19 -0800

    import google fonts and style h1 on landing

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,21 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Background from './assets/background.jpg';
+
+createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Sarala:400,700&display=swap');
+`
 
 export const LandingContainer = styled.div`
     width: 100%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 16%;
     background-image: url(${Background});
+    h1 {
+        font-size: 8rem;
+        color: white;
+        font-family: 'Sarala';
+    }
 `;
