91e78cb3ddc249842aa9683261b824df682ead54
Ben Parisot <me@benparisot.com>
2019-11-06 20:00:44 -0800

    get a funky line displaying on graph

--- src/components/d3/LineGraph.js
+++ src/components/d3/LineGraph.js
@@ -2,39 +2,80 @@ import React from 'react';
 import * as d3 from 'd3';
 
 export default function LineGraph({ data }) {
-    const width = 900;
+    const svg = d3.select('svg');
+    const color = '#CB8589';
     const height = 440;
+    const width = 1100;
 
-    const graph = d3.select('#graph');
-    console.log('graph', graph);
+    const xValue = d => d.date;
+    const xAxisLabel = 'Days';
 
-    const xScale = d3.scaleLinear()
-    .domain([0, 12])
-    .range([0, width - 100])
+    const yValue = d => d.temp;
+    const yAxisLabel = 'Temperature(F)';
+
+    const margin = { top: 60, right: 40, bottom: 88, left: 105 };
+    const innerWidth = width - (margin.left + margin.right);
+    const innerHeight = height - (margin.top + margin.bottom);
+
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(data, xValue))
+        .range([0, innerWidth])
+        .nice();
 
     const yScale = d3.scaleLinear()
-    .domain([20, 100])
-    .range([0, height]); 
-    
-    const xAxis = d3.axisBottom(xScale);
-    
-    const yAxis = d3.axisLeft(yScale);
-    
+        .domain(d3.extent(data, yValue))
+        .range([innerHeight, 0])
+        .nice();
 
-    graph.append('g')
-        .attr("transform", "translate(50, 10)")
-        .call(yAxis);
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const xAxisTranslate = height / 2 + 10;
-    
-    graph.append('g')
-        .attr("transform", "translate(50, " + xAxisTranslate  +")")
-        .call(xAxis);
+    const xAxis = d3.axisBottom(xScale)
+        .tickSize(-innerHeight)
+        .tickPadding(15);
+
+    const yAxis = d3.axisLeft(yScale)
+        .tickSize(-innerWidth)
+        .tickPadding(10);
+
+    const yAxisG = g.append('g').call(yAxis);
+    yAxisG.selectAll('.domain').remove();
+
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', -60)
+        .attr('x', -innerHeight / 2)
+        .attr('fill', 'black')
+        .attr('transform', `rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .text(yAxisLabel);
+
+    const xAxisG = g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${innerHeight})`);
+
+    xAxisG.select('.domain').remove();
+
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 80)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabel);
+
+    const lineGenerator = d3.line()
+        .x(d => xScale(xValue(d)))
+        .y(d => yScale(yValue(d)))
+        .curve(d3.curveBasis);
+
+    console.log('line', lineGenerator(data));
 
 
-    return(
-        <>
-        <svg id="graph" width="1100" height="440" />
-        </>
+
+    return (
+        <svg width={width} stroke={color} fill={color} height={height}>
+            <g>
+                <path d={lineGenerator(data)}></path>
+            </g>
+        </svg>
     )
 }
--- src/pages/DataDisplay.js
+++ src/pages/DataDisplay.js
@@ -7,18 +7,26 @@ import LineGraph from '../components/d3/LineGraph';
 export default class DataDisplay extends PureComponent {
     state = {
         data: [
-            { date: 8, temp: 41 },
-            { date: 9, temp: 44 },
-            { date: 10, temp: 48 },
-            { date: 11, temp: 53 },
-            { date: 12, temp: 57 },
+            { date: new Date("2019-11-06T19:00:00-08:00"), temp: 53 },
+            { date: new Date("2019-11-06T20:00:00-08:00"), temp: 51 },
+            { date: new Date("2019-11-06T21:00:00-08:00"), temp: 50 },
+            { date: new Date("2019-11-06T22:00:00-08:00"), temp: 49 },
+            { date: new Date("2019-11-06T23:00:00-08:00"), temp: 48 },
+            { date: new Date("2019-11-07T00:00:00-08:00"), temp: 47 },
+            { date: new Date("2019-11-07T01:00:00-08:00"), temp: 46 },
+            { date: new Date("2019-11-07T02:00:00-08:00"), temp: 45 },
+            { date: new Date("2019-11-07T03:00:00-08:00"), temp: 44 },
+            { date: new Date("2019-11-07T03:00:00-08:00"), temp: 44 },
+            { date: new Date("2019-11-07T05:00:00-08:00"), temp: 43 },
+            { date: new Date("2019-11-07T06:00:00-08:00"), temp: 41 },
+
         ]
     }
 
     
 
     render() {
-
+        const { data } = this.state;
         return(
             <>
                 <Header />
@@ -27,7 +35,7 @@ export default class DataDisplay extends PureComponent {
                     <h1 className="location-title">Portland, OR</h1>
                 </DisplayTitle>
                 <div>
-                    <LineGraph />
+                    <LineGraph data={data} />
                 </div>
                 <DataDetail>
                     <DataDescription>
