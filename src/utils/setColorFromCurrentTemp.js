b5f6277718443381d5c481afe42284b6ebd4424b
Ben Parisot <me@benparisot.com>
2019-11-09 15:21:02 -0800

    fix 60 bug on setColor from temp

--- src/utils/setColorFromCurrentTemp.js
+++ src/utils/setColorFromCurrentTemp.js
@@ -1,6 +1,6 @@
 
 export const setColorFromCurrentTemp = currentTemp => {
-    if(currentTemp < 60) return '#6FB5E8';
-    else if (currentTemp > 60 && currentTemp < 80) return '#F78E69';
+    if(currentTemp < 60 ) return '#6FB5E8';
+    else if (currentTemp >= 60 && currentTemp < 80) return '#F78E69';
     else return '#931F1D';
 }
