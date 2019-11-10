2b0c37e5662a680abc1dd55538088a210a5462ff
Ben Parisot <me@benparisot.com>
2019-11-10 11:46:42 -0800

    update fetch request urls to https

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,7 +1,7 @@
 export const fetchWeather = async (zip) => {
-    let keyResponse = await fetch(`http://dataservice.accuweather.com/locations/v1/postalcodes/US/search?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH&q=${zip}`);
+    let keyResponse = await fetch(`https://dataservice.accuweather.com/locations/v1/postalcodes/US/search?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH&q=${zip}`);
     let keyData = await keyResponse.json();
-    let weatherResponse = await fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${keyData[0].ParentCity.Key}?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH`)
+    let weatherResponse = await fetch(`https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${keyData[0].ParentCity.Key}?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH`)
     let weatherData = await weatherResponse.json();
     const weatherObject = {
         cityData: keyData,
