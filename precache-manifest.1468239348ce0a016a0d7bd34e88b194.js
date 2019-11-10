ef9775dcaa807fd4504f4b51efe10bf271a6f6cf
Ben Parisot <me@benparisot.com>
2019-11-09 16:44:16 -0800

    Updates

---
+++ precache-manifest.1468239348ce0a016a0d7bd34e88b194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1088f1b86e1e68092349ad350f60fcb3",
+    "url": "/sprinklr-takehome/public/index.html"
+  },
+  {
+    "revision": "fa83b3d09fb34a30c7f7",
+    "url": "/sprinklr-takehome/public/static/css/main.d9bba2d3.chunk.css"
+  },
+  {
+    "revision": "d04c20013312a908fcf9",
+    "url": "/sprinklr-takehome/public/static/js/2.0f571c72.chunk.js"
+  },
+  {
+    "revision": "fa83b3d09fb34a30c7f7",
+    "url": "/sprinklr-takehome/public/static/js/main.7cdb0e40.chunk.js"
+  },
+  {
+    "revision": "a87ff9e6b24205530acc",
+    "url": "/sprinklr-takehome/public/static/js/runtime-main.26ba94af.js"
+  },
+  {
+    "revision": "c5a1e20af1d09a3fc6dc8194b18ad7ff",
+    "url": "/sprinklr-takehome/public/static/media/background.c5a1e20a.jpg"
+  },
+  {
+    "revision": "d411451b7fbe4557651c6eab1491fce3",
+    "url": "/sprinklr-takehome/public/static/media/header-background.d411451b.jpg"
+  }
+]);
