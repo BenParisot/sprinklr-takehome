06c0c00a77d5062b9dbf3ea61cfb532a3df75a5d
Ben Parisot <me@benparisot.com>
2020-01-27 09:24:37 -0800

    update accuweather

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,12 +1,12 @@
 export const fetchWeather = async (zip) => {
-    let keyResponse = await fetch(`https://dataservice.accuweather.com/locations/v1/postalcodes/US/search?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH&q=${zip}`);
+    let keyResponse = await fetch(`https://dataservice.accuweather.com/locations/v1/postalcodes/US/search?apikey=lQQRp0lpAFwmdth6WndYEFAZI8Z2EcVl=${zip}`);
     console.log('key response', typeof(keyResponse.status));
     if (keyResponse.status !== 200) return keyResponse.status;
     else {
         let keyData = await keyResponse.json();
         if (keyData.length === 0) return null;
         else {
-            let weatherResponse = await fetch(`https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${keyData[0].ParentCity.Key}?apikey=GkyViiHRqGoeGWJIsYJkl27SxwiH3DXH`)
+            let weatherResponse = await fetch(`https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${keyData[0].ParentCity.Key}?apikey=lQQRp0lpAFwmdth6WndYEFAZI8Z2EcVl`)
             let weatherData = await weatherResponse.json();
             const weatherObject = {
                 cityData: keyData,
